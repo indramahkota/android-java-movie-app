4fa8318066dc2d6d3c100fa57be6f5aed1f332c5
Indra Mahkota <indramahkota1@gmail.com>
2019-11-18 19:34:40 -0800

    add

--- app/src/main/java/com/indramahkota/moviecatalogue/ui/splash/SplashActivity.java
+++ app/src/main/java/com/indramahkota/moviecatalogue/ui/splash/SplashActivity.java
@@ -1,18 +1,39 @@
 package com.indramahkota.moviecatalogue.ui.splash;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.indramahkota.moviecatalogue.ui.main.MainActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final String STATE_HANDLER = "state_handler";
+
+    private Boolean stateHandler = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashActivity.this, MainActivity.class));
-        finish();
+
+        if (savedInstanceState != null) {
+            stateHandler = savedInstanceState.getBoolean(STATE_HANDLER);
+        }
+
+        if(!stateHandler) {
+            stateHandler = true;
+            final Handler handler = new Handler();
+            handler.postDelayed(() -> {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }, 2000);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(STATE_HANDLER, stateHandler);
     }
 }
